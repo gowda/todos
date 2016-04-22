01f894711df90200cee1ce38f3dae3cd8cf96469
Basavanagowda Kanur <basavanagowda@gmail.com>
2016-04-22 16:07:23 +0530

    feature test - test for 'POST /' for creating todo

--- src/test/scala-2.11/com/example/todo/TodoFeatureTest.scala
+++ src/test/scala-2.11/com/example/todo/TodoFeatureTest.scala
@@ -3,9 +3,53 @@ package com.example.todo
 import com.twitter.finagle.http.Status
 import com.twitter.finatra.http.test.EmbeddedHttpServer
 import com.twitter.inject.server.FeatureTest
+import org.joda.time.DateTime
 
 class TodoFeatureTest extends FeatureTest {
   override val server = new EmbeddedHttpServer(new TodoServer)
+  val invalidJson = s"""{}"""
+  val todoJson1 = s"""
+                 |{
+                 |   "title": "Test task",
+                 |   "description": "Write tests for creating tasks",
+                 |   "start": "${DateTime.now().plusDays(5).toString}",
+                 |   "end": "${DateTime.now().plusDays(6).toString}"
+                 |}
+          """.stripMargin
+  val todoJsons = List(
+    s"""
+       |{
+       |   "title": "Test task - 1",
+       |   "description": "Write tests for creating tasks - 1",
+       |   "start": "${DateTime.now().plusDays(1).toString}",
+       |   "end": "${DateTime.now().plusDays(2).toString}"
+       |}
+          """.stripMargin,
+    s"""
+       |{
+       |   "title": "Test task - 2",
+       |   "description": "Write tests for creating tasks - 2",
+       |   "start": "${DateTime.now().plusDays(3).toString}",
+       |   "end": "${DateTime.now().plusDays(4).toString}"
+       |}
+          """.stripMargin,
+    s"""
+       |{
+       |   "title": "Test task - 3",
+       |   "description": "Write tests for creating tasks - 3",
+       |   "start": "${DateTime.now().plusDays(5).toString}",
+       |   "end": "${DateTime.now().plusDays(6).toString}"
+       |}
+          """.stripMargin,
+    s"""
+       |{
+       |   "title": "Test task - 4",
+       |   "description": "Write tests for creating tasks - 4",
+       |   "start": "${DateTime.now().plusDays(7).toString}",
+       |   "end": "${DateTime.now().plusDays(8).toString}"
+       |}
+          """.stripMargin
+  )
 
   "Server" should {
     "respond with '200 OK' for GET '/'" in {
@@ -14,6 +58,34 @@ class TodoFeatureTest extends FeatureTest {
 
     "respond with empty list for GET '/', when nothing has been added" in {
       server.httpGet(path = "/", andExpect = Status.Ok, withJsonBody = "[]")
+    }
+
+    "respond with '201 Created' for POST '/'" in {
+      server.httpPost(
+        path = "/",
+        postBody = todoJson1,
+        andExpect = Status.Created)
+    }
+
+    "respond with '400 Bad Request' for POST '/', when json in invalid" in {
+      server.httpPost(
+        path = "/",
+        postBody = invalidJson,
+        andExpect = Status.BadRequest)
+    }
+
+    "respond with all todos for 'GET /'" in {
+      for (todo <- todoJsons) {
+        server.httpPost(
+          path = "/",
+          postBody = todo,
+          andExpect = Status.Created)
+      }
+
+      server.httpGet(
+        path = "/",
+        andExpect = Status.Ok,
+        withJsonBody = s"[$todoJson1, ${todoJsons.mkString(",")}]")
     }
   }
 }
