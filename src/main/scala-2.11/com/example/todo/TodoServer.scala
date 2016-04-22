2832f3c8ed980d4d71ef04b1f66396b8d0c71d20
Basavanagowda Kanur <basavanagowda@gmail.com>
2016-04-22 14:03:36 +0530

    configure server with TodoController added to router

--- src/main/scala-2.11/com/example/todo/TodoServer.scala
+++ src/main/scala-2.11/com/example/todo/TodoServer.scala
@@ -1,5 +1,10 @@
 package com.example.todo
 
 import com.twitter.finatra.http.HttpServer
+import com.twitter.finatra.http.routing.HttpRouter
 
-class TodoServer extends HttpServer
+class TodoServer extends HttpServer {
+  override def configureHttp(router: HttpRouter): Unit = {
+    router.add[TodoController]
+  }
+}
