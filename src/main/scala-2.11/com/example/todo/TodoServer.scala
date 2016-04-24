23cb5ce4519aba318d62aa176db5d7694e48ba04
Basavanagowda Kanur <basavanagowda@gmail.com>
2016-04-24 13:01:53 +0530

    server - add finatra/CommonFilters to the router

--- src/main/scala-2.11/com/example/todo/TodoServer.scala
+++ src/main/scala-2.11/com/example/todo/TodoServer.scala
@@ -2,12 +2,15 @@ package com.example.todo
 
 import com.example.todo.modules.JedisModule
 import com.twitter.finatra.http.HttpServer
+import com.twitter.finatra.http.filters.{CommonFilters, ExceptionMappingFilter}
 import com.twitter.finatra.http.routing.HttpRouter
 
 class TodoServer extends HttpServer {
   override def modules = Seq(JedisModule)
 
   override def configureHttp(router: HttpRouter): Unit = {
-    router.add[TodoController]
+    router
+      .filter[CommonFilters]
+      .add[TodoController]
   }
 }
