62932b022ade89fd105c486f9c11da38fe4053dc
Basavanagowda Kanur <basavanagowda@gmail.com>
2016-04-24 13:02:45 +0530

    request object - set properties to be ignored while serializing

--- src/main/scala-2.11/com/example/todo/PostTodoRequest.scala
+++ src/main/scala-2.11/com/example/todo/PostTodoRequest.scala
@@ -2,9 +2,11 @@ package com.example.todo
 
 import javax.inject.Inject
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.twitter.finagle.http.Request
 import org.joda.time.DateTime
 
+@JsonIgnoreProperties(Array("request"))
 case class PostTodoRequest(@Inject request: Request,
                            title: String,
                            description: String,
