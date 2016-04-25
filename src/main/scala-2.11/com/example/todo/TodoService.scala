4e03becb6f59703f7c9d69958d72c0dcecc84887
Basavanagowda Kanur <basavanagowda@gmail.com>
2016-04-25 14:11:16 +0530

    TodoService - use redis as persisting store

--- src/main/scala-2.11/com/example/todo/TodoService.scala
+++ src/main/scala-2.11/com/example/todo/TodoService.scala
@@ -1,15 +1,33 @@
 package com.example.todo
 
-class TodoService {
-  var todos: List[Todo] = List()
+import javax.inject.Inject
 
+import com.twitter.finatra.json.FinatraObjectMapper
+import com.twitter.inject.Logging
+import redis.clients.jedis.Jedis
+import com.example.todo.TodoIdCounter.KeyPrefix
+
+class TodoService @Inject()(client: Jedis,
+                            counter: TodoIdCounter,
+                            mapper: FinatraObjectMapper)
+  extends Logging {
   def getAll(): List[Todo] = {
+    val keys = client.keys(s"$KeyPrefix-*").toArray
+    var todos: List[Todo] = List[Todo]()
+    for (key <- keys) {
+      val todoJson = client.get(key.toString)
+      info(s"Key: $key")
+      info(s"JSON read from redis: $todoJson")
+      val todo: Todo = mapper.parse[Todo](todoJson)
+      todos = todo :: todos
+    }
+
     todos
   }
 
   def add(request: PostTodoRequest): Todo = {
-    val todo = Todo(request.title, request.description, request.start, request.end)
-    todos ++= List(todo)
-    todo
+    val id = counter.next
+    client.set(id, mapper.writeValueAsString(request))
+    Todo(request.title, request.description, request.start, request.end)
   }
 }
