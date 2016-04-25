6ced4d809d41966a6ead86b93dc4ac6b9313a513
Basavanagowda Kanur <basavanagowda@gmail.com>
2016-04-25 20:01:48 +0530

    server - add custom exception mapper to exception-manager

--- src/main/scala-2.11/com/example/todo/TodoServer.scala
+++ src/main/scala-2.11/com/example/todo/TodoServer.scala
@@ -2,13 +2,17 @@ package com.example.todo
 
 import com.example.todo.modules.JedisModule
 import com.twitter.finatra.http.HttpServer
-import com.twitter.finatra.http.filters.{CommonFilters, ExceptionMappingFilter}
+import com.twitter.finatra.http.filters.CommonFilters
+import com.twitter.finatra.http.internal.exceptions.ExceptionManager
 import com.twitter.finatra.http.routing.HttpRouter
 
 class TodoServer extends HttpServer {
   override def modules = Seq(JedisModule)
 
   override def configureHttp(router: HttpRouter): Unit = {
+    val exceptionManager = injector.instance[ExceptionManager]
+    exceptionManager.add[ResourceNotFoundExceptionMapper]
+
     router
       .filter[CommonFilters]
       .add[TodoController]
