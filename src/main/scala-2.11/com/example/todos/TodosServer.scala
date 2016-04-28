2c6df68987537e3594f66893816b2266ef73bb1c
Basavanagowda Kanur <basavanagowda@gmail.com>
2016-04-28 13:34:03 +0530

    server - configure to run table creation handler at warmup

--- src/main/scala-2.11/com/example/todos/TodosServer.scala
+++ src/main/scala-2.11/com/example/todos/TodosServer.scala
@@ -2,6 +2,7 @@ package com.example.todos
 
 import com.example.todos.exceptions.ResourceNotFoundExceptionMapper
 import com.example.todos.modules.JedisModule
+import com.example.todos.storage.DatabaseConfigurationHandler
 import com.twitter.finatra.http.HttpServer
 import com.twitter.finatra.http.filters.CommonFilters
 import com.twitter.finatra.http.internal.exceptions.ExceptionManager
@@ -17,5 +18,9 @@ class TodosServer extends HttpServer {
     router
       .filter[CommonFilters]
       .add[TodosController]
+  }
+
+  override def warmup() = {
+    run[DatabaseConfigurationHandler]()
   }
 }
